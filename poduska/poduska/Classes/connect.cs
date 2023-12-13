f68b701eb702583c7d6f79399c57ffec62eff0c1
Dmitry <dmitrie.eremin.04@gmail.com>
2023-12-13 18:45:26 +0300

    poduska

---
+++ poduska/poduska/Classes/connect.cs
@@ -0,0 +1,14 @@
+﻿using poduska.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poduska.Classes
+{
+    internal class connect
+    {
+        public static pod modelbd;
+    }
+}
---
+++ poduska/poduska/Pages/EditOrders.xaml.cs
@@ -0,0 +1,82 @@
+﻿using poduska.Classes;
+using poduska.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace poduska.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditOrders.xaml
+    /// </summary>
+    public partial class EditOrders : Page
+    {
+        private orderer currentorder = new orderer();
+        public EditOrders(orderer sellectedOrder)
+
+        {
+            InitializeComponent();
+            if (sellectedOrder != null)
+            {
+                currentorder = sellectedOrder;
+            }
+            DataContext = currentorder;
+        }
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            string[] propertiesToCheck = { currentorder.id_users.ToString(), currentorder.id_status.ToString(), currentorder.id_point.ToString() };
+
+            string[] propertyNames = { "Укажите клиента", "Укажите статус", "Укажите пункт выдачи" };
+
+            for (int i = 0; i < propertiesToCheck.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(propertiesToCheck[i]))
+                {
+                    errors.AppendLine(propertyNames[i]);
+                }
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                if (currentorder.id == 0)
+                {
+                    pod.GetContext().orderer.Add(currentorder);
+                }
+
+                using (DbContextTransaction dbContextTransaction = pod.GetContext().Database.BeginTransaction())
+                {
+                    pod.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!");
+                    dbContextTransaction.Commit();
+                }
+
+                manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
+
